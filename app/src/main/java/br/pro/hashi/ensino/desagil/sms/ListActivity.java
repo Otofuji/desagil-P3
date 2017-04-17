4fcdbce388476a983fca2b78b1629da962c6dd66
Vitória Helena <vitoriahmc@al.insper.edu.br>
2017-04-17 10:24:26 -0300

    envia/recebe sms

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/sms/ListActivity.java
@@ -0,0 +1,20 @@
+package br.pro.hashi.ensino.desagil.sms;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class ListActivity extends AppCompatActivity {
+    
+    }
+}
