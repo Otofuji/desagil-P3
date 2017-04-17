57cb1b15bd0ab59c4442058e067094a9d33617f2
BrunaKimura <brunamk@al.insper.edu.br>
2017-04-17 11:23:40 -0300

    juntando as partes

--- app/src/main/java/br/pro/hashi/ensino/desagil/sms/ListActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/sms/ListActivity.java
@@ -1,20 +1,25 @@
 package br.pro.hashi.ensino.desagil.sms;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
 
 public class ListActivity extends AppCompatActivity {
-    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        // Pega o objeto que representa a listView
+        ListView listaMensagem = (ListView) findViewById(R.id.lista_mensagem);
+        //Cria o objeto com as strings
+        MessageList a = new MessageList();
+        //Adaptador para a listview(como vai ser exibida)
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a.getListaMensagem());
+        listaMensagem.setAdapter(adapter);
     }
 }
