fa5cfe58025cc11564800a3823f9d5850ec56cf9
BrunaKimura <brunamk@al.insper.edu.br>
2017-04-24 14:29:01 -0300

    arvore cheia de erros

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/MorseTree.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MorseTree.java
@@ -4,4 +4,14 @@ public class MorseTree {
     public char translate(String code) {
         return '?';
     }
-}
+
+    public MorseTree(){
+        char [] letras = new char[] {'e', 't', 'i','a', 'n', 'm','s'};
+
+        for(int i = letras.length, letras.length() <= 0, i++){
+        }
+        Node[] nodes = new Node[]
+    }
+
+
+    }
