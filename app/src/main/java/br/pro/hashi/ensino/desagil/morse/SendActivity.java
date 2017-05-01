0fcd8a08241d628ddc8003c4bcfeb325ca4574b4
Otofuji <ericfoa@al.insper.edu.br>
2017-05-01 11:30:12 -0300

    Salvando estado atual para dar fetch

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/SendActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/SendActivity.java
@@ -56,6 +56,28 @@ public class SendActivity extends AppCompatActivity {
         mensagemDaLista= extras.getString("mensagemSelecionada");
         messageEdit.append(mensagemDaLista);
 
+        this.send = false;
+        this.startTouching = 0;
+        this.startNotTouching = 0;
+        this.touching = 0;
+        this.notTouching = 0;
+        this.morseText = new ArrayList<String>();
+        this.morseTree = new MorseTree();
+        this.phrase = "";
+        this.phraseView = (TextView) findViewById(id.textMorse);
+        this.phraseViewTouch = (TextView) findViewById(id.textMorse);
+        this.phoneView = (TextView) findViewById(id.textPhone);
+        this.touchWarning = (ImageView) findViewById(id.touchWarning);
+        this.sendButton = (ImageButton) findViewById(id.sendButton);
+        this.backspace = (ImageButton) findViewById(id.backspace);
+        this.menuButton = (ImageButton) findViewById(id.menuButton);
+        this.contactButton = (ImageButton) findViewById(id.contactButton);
+        this.contactList = (ListView) findViewById(id.contactList);
+        this.messageList = (ListView) findViewById(id.messageList);
+        this.touchView = (View) findViewById(id.touchView);
+        this.all = (View) findViewById(id.all);
+        this.fromContact = false;
+
 
 
         countdown = new CountDownTimer(2*unit, 100) {
@@ -63,119 +85,148 @@ public class SendActivity extends AppCompatActivity {
             public void onTick(long millisUntilFinished) {}
             @Override
             public void onFinish() {
-
-                morseText.add(" ");
-                phrase += morseTree.translate(morseText);
-                phraseView.setText(phrase);
-                morseText.clear();
-
+                if (!send) {
+                    morseText.add(" ");
+                    phrase += decoder.decodeMorse(morseText);
+                    phraseView.setText(phrase);
+                    morseText.clear();
+                }
+                else{
+                    Log.e("MorseTouch", "NumSpace");
+                    if (morseText.size() != 5) {
+                        //Toast.makeText(MorseTouch.this, "Invalid Digit", Toast.LENGTH_SHORT).show();
+                        morseText.clear();
+                    } else {
+                        morseText.add(" ");
+                        phrase += decoder.decodeMorse(morseText);
+                        phoneView.setText(phrase);
+                        morseText.clear();
+                    }
                 }
             }
         };
+        all.setOnClickListener(null);
+    }
 
+
+    public SendActivity(EditText numberEdit) {
+        this.numberEdit = numberEdit;
+    }
 
     public boolean onTouch(View touchView, MotionEvent event) {
+        if(fromContact){
+            Toast.makeText(MorseTouch.this, "You have already chosen a contact", Toast.LENGTH_SHORT).show();
+        }
+        if (!startRecording) {
+            this.startRecording = true;
+            phraseViewTouch.setText("");
+        }
+        if (startRecording) {
+            Log.e("MorseTouch", String.valueOf(event.getActionMasked()));
+            int action = event.getAction() & MotionEvent.ACTION_MASK;
+            if (event.getAction() == MotionEvent.ACTION_DOWN || event.getActionMasked() == MotionEvent.ACTION_POINTER_DOWN) {
+                setActivityBackgroundColor(Color.parseColor("#2C2C2C"));
+                this.startTouching = 0;
+                this.touching = 0;
+                startTouching = System.currentTimeMillis();
+                notTouching = System.currentTimeMillis() - startNotTouching;
+                countdown.cancel();
+                return true;
+            }
 
-
-        Log.e("SendActivity", String.valueOf(event.getActionMasked()));
-        int action = event.getAction() & MotionEvent.ACTION_MASK;
-        if (event.getAction() == MotionEvent.ACTION_DOWN || event.getActionMasked() == MotionEvent.ACTION_POINTER_DOWN) {
-
-            this.startTouching = 0;
-            this.touching = 0;
-            startTouching = System.currentTimeMillis();
-            notTouching = System.currentTimeMillis() - startNotTouching;
-            countdown.cancel();
-            return true;
+            if (event.getAction() == MotionEvent.ACTION_UP || event.getActionMasked() == MotionEvent.ACTION_POINTER_UP) {
+                setActivityBackgroundColor(Color.parseColor("#E0E0E0"));
+                touching = System.currentTimeMillis() - startTouching;
+                startNotTouching = System.currentTimeMillis();
+                countdown.start();
+            }
         }
 
-        if (event.getAction() == MotionEvent.ACTION_UP || event.getActionMasked() == MotionEvent.ACTION_POINTER_UP) {
-
-            touching = System.currentTimeMillis() - startTouching;
-            startNotTouching = System.currentTimeMillis();
-            countdown.start();
-        }
-
-
-
-        if (touching > 0 && notTouching > 0) {
-            if (touching > 5 * unit) {
-                Log.e("SendActivity", "EndPhrase");
-                morseText.add(" ");
-                phrase += morseTree.translate(morseText);
-                phrase += ".";
-                phraseView.setText(phrase);
-                Log.e("SendActivity", phrase);
-                morseText.clear();
-            } else {
-                //End Word
-                if (notTouching > 6 * unit && notTouching < 100 * unit) {
-                    Log.e("SendActivity", "EndWord");
-                    Log.e("SendActivity", "Space");
+        if (!send) {
+            if (touching > 0 && notTouching > 0) {
+                if (touching > 5 * unit) {
+                    Log.e("MorseTouch", "EndPhrase");
                     morseText.add(" ");
-                    Log.e("SendActivity", morseText.toString());
-                    phrase += morseTree.translate(morseText);
-                    phrase += " ";
+                    phrase += decoder.decodeMorse(morseText);
+                    phrase += ".";
                     phraseView.setText(phrase);
-                    Log.e("SendActivity", phrase);
+                    Log.e("MorseTouch", phrase);
+                    morseText.clear();
+                } else {
+                    //End Word
+                    if (notTouching > 6 * unit && notTouching < 100 * unit) {
+                        Log.e("MorseTouch", "EndWord");
+                        Log.e("MorseTouch", "Space");
+                        morseText.add(" ");
+                        Log.e("MorseTouch", morseText.toString());
+                        phrase += decoder.decodeMorse(morseText);
+                        phrase += " ";
+                        phraseView.setText(phrase);
+                        Log.e("MorseTouch", phrase);
+                        morseText.clear();
+                    }
+                    //Space
+                    if (notTouching > 2 * unit && notTouching < 6 * unit) {
+                        Log.e("MorseTouch", "Space");
+                        morseText.add(" ");
+                        phrase += decoder.decodeMorse(morseText);
+                        phraseView.setText(phrase);
+                        morseText.clear();
+                    }
+                    //LongPress
+                    if (touching > unit && touching < 5 * unit) {
+                        Log.e("MorseTouch", "LongPress");
+                        morseText.add("-");
+                    }
+                    //ShortPress
+                    if (touching < unit) {
+                        Log.e("MorseTouch", "ShortPress");
+                        morseText.add(".");
+                    }
+                }
+            }
+        } else {
+            if (touching > 0 && notTouching > 0) {
+                if (touching > 5 * unit) {
+                    Log.e("MorseTouch", "EndPhone");
+                    morseText.add(" ");
+                    phrase += decoder.decodeMorse(morseText);
+                    phoneView.setText(phrase);
+                    Toast.makeText(MorseTouch.this, phrase, Toast.LENGTH_SHORT).show();
+                    Log.e("MorseTouch", phrase);
                     morseText.clear();
                 }
                 //Space
-                if (notTouching > 2 * unit && notTouching < 6 * unit) {
-                    Log.e("SendActivity", "Space");
-                    morseText.add(" ");
-                    phrase += morseTree.translate(morseText);
-                    phraseView.setText(phrase);
-                    morseText.clear();
+                if (notTouching > 2 * unit) {
+                    Log.e("MorseTouch", "NumSpace");
+                    if (morseText.size() != 5) {
+                        //Toast.makeText(MorseTouch.this, "Invalid Digit", Toast.LENGTH_SHORT).show();
+                        morseText.clear();
+                    } else {
+                        morseText.add(" ");
+                        phrase += decoder.decodeMorse(morseText);
+                        phoneView.setText(phrase);
+                        morseText.clear();
+                    }
                 }
                 //LongPress
                 if (touching > unit && touching < 5 * unit) {
-                    Log.e("SendActivity", "LongPress");
+                    Log.e("MorseTouch", "NumLongPress");
                     morseText.add("-");
                 }
                 //ShortPress
                 if (touching < unit) {
-                    Log.e("SendActivity", "ShortPress");
+                    Log.e("MorseTouch", "NumShortPress");
                     morseText.add(".");
                 }
             }
         }
-
-        if (touching > 5 * unit) {
-            Log.e("SendActivity", "EndPhone");
-            morseText.add(" ");
-            phrase += morseTree.translate(morseText);
-            phoneView.setText(phrase);
-            Toast.makeText(SendActivity.this, phrase, Toast.LENGTH_SHORT).show();
-            Log.e("SendActivity", phrase);
-            morseText.clear();
-        }
-        //Space
-        if (notTouching > 2 * unit) {
-            Log.e("SendActivity", "NumSpace");
-            if (morseText.size() != 5) {
-                //Toast.makeText(SendActivity.this, "Invalid Digit", Toast.LENGTH_SHORT).show();
-                morseText.clear();
-            } else {
-                morseText.add(" ");
-                phrase += morseTree.translate(morseText);
-                phoneView.setText(phrase);
-                morseText.clear();
-            }
-        }
-        //LongPress
-        if (touching > unit && touching < 5 * unit) {
-            Log.e("SendActivity", "NumLongPress");
-            morseText.add("-");
-        }
-        //ShortPress
-        if (touching < unit) {
-            Log.e("SendActivity", "NumShortPress");
-            morseText.add(".");
-        }
-
-
         return false;
+    }
+
+    public void setActivityBackgroundColor(int color) {
+        View view = findViewById(id.thirdLayout);
+        view.setBackgroundColor(color);
     }
 
     public void sendMessage(View view) {
